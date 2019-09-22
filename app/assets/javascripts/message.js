ed881e7415fe3df4497c8b5bbccd6c046337c483
Ryodai Hayashi <ryoudai0221@gmail.com>
2019-09-22 12:00:56 +0900

    revised

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,22 +4,21 @@ $(function() {
     image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} >` : "";
   
     var html = `<div class="message" data-message-id="${message.id}">
-  <div class="upper-message">
-  <div class="upper-message__user-name">
-    ${message.user_name}
-  </div>
-  <div class="upper-message__date">
-    ${message.date}
-  </div>
-  </div>
-  <div class="lower-message">
-  <p class="lower-message__content">
-    ${message.content}
-  </p>
-  </div>
-   ${image}
-  </div>`
-
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.date}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            ${image}
+          </div>
+        </div>`
     return html;
 }
 
@@ -75,9 +74,8 @@ $.ajax({
       })
       .fail(function () {
         alert('自動更新に失敗しました');
-        });
-      }
+         });
+        }
       };
       setInterval(reloadMessages, 5000);
       });
-    
